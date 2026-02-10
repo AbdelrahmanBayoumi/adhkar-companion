5ad179e95c72b607f2662117c315f0bdbb73bf92
Abdelrahman Bayoumi <abdelrahmanbayoumi1@gmail.com>
2026-02-11 00:22:25 +0200

    fix: add router basename for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AppProvider } from '@/contexts/AppContext';
-import Index from './pages/Index';
-import NotFound from './pages/NotFound';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AppProvider } from "@/contexts/AppContext";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
 
 const App = () => (
   <AppProvider>
-    <BrowserRouter>
+    <BrowserRouter basename="/adhkar-companion">
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="*" element={<NotFound />} />
